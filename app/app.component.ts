534a1ca49a5cb37534709bd794f2b54efa5209e8
ephie <ephie@localhost.localdomain>
2017-07-03 12:00:43 +0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+//annotation//includes a selector and a template.
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>My First Angular 2 App</h1>
+  `
+})
+//class//making it available toother components
+export class AppComponent {
+
+}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+//Responsible for loading the AppModule file
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
